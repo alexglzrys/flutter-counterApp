05399b51175b869058baf70b03c5c4547fad7c76
Alejandro González Reyes <alexglzrys@gmail.com>
2022-02-06 17:43:24 -0600

    optimizaciones y pasar funciones como argumentos

--- lib/screens/counter_screen.dart
+++ lib/screens/counter_screen.dart
@@ -16,6 +16,24 @@ class _CounterScreenState extends State<CounterScreen> {
   // Mi estado inicial del contador
   int counter = 0;
 
+  // Funciones encargadas de gestionar el estado, las cuales serán pasadas como parámetro
+  // al Widget encargado de dibujar los FloatingActionButton,
+  // para que se pueda se pueda comunicar con el estado de este Widget
+  void increase() {
+    counter++;
+    setState(() {});
+  }
+
+  void decrease() {
+    counter--;
+    setState(() {});
+  }
+
+  void reset() {
+    counter = 0;
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     // BuildContext: hace referencia al árbol de Widgets que tiene mi App (tiene info del contexto donde se esta colocando el Widget actual)
@@ -33,44 +51,9 @@ class _CounterScreenState extends State<CounterScreen> {
         floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
         // Boton flotante (pero en esa posición se puede colocar cualquier Widget)
         // Para este ejemplo tenemos 3 botones flotantes agrupados en una fila
-        floatingActionButton: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            FloatingActionButton(
-              onPressed: () {
-                // Cambio el estado
-                counter--;
-                // Indicar a Flutter que el estado ha cambiado, por tanto redibuje los cambios en pantalla para este Widget
-                setState(() {});
-              },
-              child: const Icon(Icons.exposure_minus_1),
-            ),
-
-            // ? Una técnica utilizada para separar Widgets es usar el Widget.
-            /*const SizedBox(
-              width: 30.0,
-            ),*/
-
-            FloatingActionButton(
-              onPressed: () {
-                // Cambio el estado
-                counter = 0;
-                // Indicar a Flutter que el estado ha cambiado, por tanto redibuje los cambios en pantalla para este Widget
-                setState(() {});
-              },
-              child: const Icon(Icons.restore),
-            ),
-            FloatingActionButton(
-              onPressed: () {
-                // Cambio el estado
-                counter++;
-                // Indicar a Flutter que el estado ha cambiado, por tanto redibuje los cambios en pantalla para este Widget
-                setState(() {});
-              },
-              child: const Icon(Icons.plus_one),
-            ),
-          ],
-        ),
+        // Pasamos las funciones encargadas de gestionar el estado, para que en el componente externo se puedan invocar y el estado pueda cambiar. ya que el estado sólo esta presente dentro de este componente
+        floatingActionButton: FloatingCustomActionButton(
+            increaseFn: increase, decreaseFn: decrease, resetFn: reset),
         body: Center(
             child: Column(
           // Alinear la columna al centro en su eje principal (eje Y) - Posicionar el puntero sobre la propiedad nos ayuda a saber que tipo de valor requiere
@@ -97,5 +80,47 @@ class _CounterScreenState extends State<CounterScreen> {
             )
           ],
         )));
+  }
+}
+
+class FloatingCustomActionButton extends StatelessWidget {
+  // Propiedades que guardan una referencia a las funciones que gestionan el estado del componente CounterScreen
+  final Function increaseFn;
+  final Function decreaseFn;
+  final Function resetFn;
+
+  const FloatingCustomActionButton({
+    Key? key,
+    required this.increaseFn,
+    required this.decreaseFn,
+    required this.resetFn,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        FloatingActionButton(
+          // Invocar las funciones para decirles que modifiquen el estado
+          onPressed: () => decreaseFn(),
+          child: const Icon(Icons.exposure_minus_1),
+        ),
+
+        // ? Una técnica utilizada para separar Widgets es usar el Widget.
+        /*const SizedBox(
+          width: 30.0,
+        ),*/
+
+        FloatingActionButton(
+          onPressed: () => resetFn(),
+          child: const Icon(Icons.restore),
+        ),
+        FloatingActionButton(
+          onPressed: () => increaseFn(),
+          child: const Icon(Icons.plus_one),
+        ),
+      ],
+    );
   }
 }
