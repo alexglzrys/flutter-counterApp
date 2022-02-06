e0fbe8fbe9d0e64345c2646c557bc06d3ecd182d
Alejandro González Reyes <alexglzrys@gmail.com>
2022-02-06 14:25:14 -0600

    prefer const

--- lib/main.dart
+++ lib/main.dart
@@ -3,17 +3,22 @@ import 'package:flutter/material.dart';
 // Entrada principal de la aplicación
 void main() {
   // Correr el App (Todo en Flutter es un Widget)
-  runApp(MyApp());
+  // Si el editor nos aconseja trabajar con constructores constantes al momento de instanciar, debemos hacerlo. Esto ayuda al rendimiento de nuestra app
+  runApp(const MyApp());
 }
 
 // Los Widgets son clases que extienden de la clase padre Widget, que puede ser con estado o sin estado
 class MyApp extends StatelessWidget {
+  // Se recomienda seguir las ayudas del IDE para agregar elementos que considera importantes en la construcción de nuestra clase
+  const MyApp({Key? key}) : super(key: key);
+
   // Tdoo widget tiene el método build, el cual se debe sobreescribir
   @override
   Widget build(BuildContext context) {
     // Construir el cuerpo de nuestra aplicación
-    return MaterialApp(
-      home: Text('Hola mundo'),
+    // Se recomienda instanciar nuestras clases padre como constantes si sus hijos nunca van a cambiar, para ello no podemos valer de las ayudas del IDE
+    return const MaterialApp(
+      home: Center(child: Text('Hola mundo')),
     );
   }
 }
